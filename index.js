b8c5f08f78a004a5268f6eef7dd1675dfbf1cc4c
temuriusa <temuriusa@gmail.com>
2022-06-04 01:32:36 +0200

    ajc-bot

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+client.once('ready', () => {
+    console.log(`ready`)
+})
+client.on('message', message => {
+    console.log(message.content);
+
+    if(message.content.startsWith(`${prefix}code`)) { 
+        message.channel.send('le code de aujourdhui  est: 7324 ')
+    } else if (message.content.startsWith(`${prefix}natalie`)) {
+        message.channel.send('Big Brother is Watching You !')
+    }
+})
+client.login(token)
